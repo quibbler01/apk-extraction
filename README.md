7cc9b97d9811352f38a24fef666d7e4cd4d53080
quibbler01 <a2639168845@163.com>
2021-08-05 20:28:47 +0800

    apk提取脚本雏形

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # apk-extraction
-apk extraction script
+apk extraction script,
+A script to facilitate the developer to extract the application installation package from the mobile phone
+see Quibbler.cn for details.
---
+++ apk_extraction.sh
@@ -0,0 +1,40 @@
+#Copyright [2021] [Quibbler.cn]
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+
+#current focused App, output: mCurrentFocus=Window{c053bb9 u0 com.ss.android.ugc.aweme/com.ss.android.ugc.aweme.splash.SplashActivity}
+focus=`adb shell dumpsys window | findStr mCurrentFocus`
+
+
+#extract out {package/class}, output: com.ss.android.ugc.aweme/com.ss.android.ugc.aweme.splash.SplashActivity
+classpath=`echo ${focus} | awk '{print $3}'`
+
+
+#crop string extract package name, output: com.ss.android.ugc.aweme
+package=`echo ${classpath%/*}`
+
+
+#figure out this package install path, output: package:/data/app/com.ss.android.ugc.aweme-bWD-6eHGY_JWxoiHreHc5A==/base.apk
+path=`adb shell pm path $package`
+#sub path from pm output's, output: data/app/com.ss.android.ugc.aweme-bWD-6eHGY_JWxoiHreHc5A==/base.apk
+path=`echo ${path#*/}`
+#and replace '/' with '//' if path include '/base.apk',E.g: data/app/com.vivo.easyshare-Q46ic5yVLxvgkiPRkB318Q==//base.apk
+if [[ $path =~ /base.apk ]]; then
+	path=`echo ${path///base.apk///base.apk}`
+fi
+
+
+#pull apk out renanme with package name
+adb pull $path  $package.apk
+
